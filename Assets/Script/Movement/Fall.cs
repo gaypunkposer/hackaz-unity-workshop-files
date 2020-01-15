7b0ad7b3ffdec5509c21c20277c2fe8758a59a23
Jessica Coan <jcoan188@gmail.com>
2020-01-15 09:38:09 -0700

    Importing files from 3d game kit

---
+++ Assets/Script/Movement/Fall.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Workshop.Player.Movement
+{
+    public class Fall : MonoBehaviour
+    {
+        public PlayerController Controller;
+        public float MoveSpeed;
+        public float Weight;
+        public float Gravity;
+        public float AirResistance;
+
+        void Start()
+        {
+            if (!Controller)
+            {
+                Debug.LogError("Controller not set on Fall! Disabling to prevent further errors.");
+                enabled = false;
+            }
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (!Controller.IsGrounded())
+            {
+                Vector3 input = Controller.GetInput();
+                input.y = 0;
+                input.Normalize();
+
+                Vector3 desiredMove = transform.TransformDirection(input) * MoveSpeed;
+                desiredMove -= Controller.Velocity * AirResistance;
+                desiredMove /= Weight / Time.deltaTime;
+                desiredMove.y -= Gravity * Time.deltaTime;
+
+                Controller.MoveCharacter(desiredMove);
+            }
+        }
+    }
+}
---
+++ Assets/Script/Movement/Jump.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Workshop.Player.Movement;
+
+namespace Workshop.Player.Movement
+{
+    public class Jump : MonoBehaviour
+    {
+        public PlayerController Controller;
+        public float JumpThrust;
+        public int MaxJumps;
+
+        private float m_jumpCount;
+        void Start()
+        {
+            if (!Controller)
+            {
+                Debug.LogError("Controller not set on Jump! Disabling to prevent further errors.");
+                enabled = false;
+            }
+        }
+
+        void Update()
+        {
+            //If the player is grounded, reset the number of times they can jump.
+            if (Controller.IsGrounded())
+            {
+                m_jumpCount = 0;
+            }
+
+            Vector3 input = Controller.GetInput();
+            
+            //If the jump button has been pressed and we can jump more, jump.
+            if (input.y > 0 && m_jumpCount < MaxJumps)
+            {
+                m_jumpCount++;
+                Controller.MoveCharacter(new Vector3(0, JumpThrust, 0));
+            }
+        }
+    }
+}
